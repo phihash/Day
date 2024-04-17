6e7906cb4843069e784ca8d3a4960126c5c7440b
phihash <insharp0220@gmail.com>
2024-04-17 22:50:33 +0900

    せってい

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { noto_sans_jp,geo, arvo ,zen_maru_gothic, lexend } from "../font";
 
 async function getWikiData() {
   try {
@@ -34,6 +35,44 @@ const events = extract.split('\n').filter((event : any) => {
   }
 }
 
+async function getBirthdaysData(month: number, day: number) {
+  try {
+    const response = await axios.get('https://ja.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        prop: 'extracts',
+        titles: `${month}月${day}日`,
+        exintro: true,
+        explaintext: true,
+      },
+    });
+    console.log(response,"レスポン")
+    const data = response.data;
+    console.log(data,"data")
+    const pages = data.query.pages;
+    console.log(pages,"page")
+    const pageId = Object.keys(pages)[0];
+    console.log(pageId,"pageId")
+    const extract = pages[pageId].extract;
+    console.log(extract,"extrac")
+
+    // 誕生日の人物のみを抽出
+    const regex = /(\d+年)(?:.+?)、(.+?)(?:（.+?）)?が生まれる。/g;
+    const birthdays = [];
+    let match;
+    while ((match = regex.exec(extract)) !== null) {
+      const [_, year, name] = match;
+      birthdays.push({ year, name });
+    }
+
+    return birthdays;
+  } catch (error) {
+    console.error(error);
+    throw new Error('An error occurred');
+  }
+}
+
 export default async function Home() {
   const wikiData = await getWikiData();
 
@@ -42,13 +81,15 @@ export default async function Home() {
     const year = currentDate.getFullYear();
     const month = currentDate.getMonth() + 1;
     const day = currentDate.getDate();
+    const birthdays = await getBirthdaysData(month, day);
+
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl">
-      {year}年{month}月{day}日
-      </h1>
-      <pre>{wikiData}</pre>
-    </main>
+    <main className="flex h-1/2 flex-col text-neutral-100 items-center justify-between p-24 pt-48 bg-yellow-500">
+    <h1 className={`text-4xl font-bold ${arvo.className}`}>
+    {year}/{month}/{day}
+    </h1>
+
+  </main>
   );
 }

99441efa2b6f412d22627d9ed2f4df93cd3376c2
phihash <insharp0220@gmail.com>
2024-04-18 01:28:41 +0900

    ファイルの分離

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { calculateRokuyou } from "../libs/calculateRokuyou"
-import {lato, potta_One } from "../font";
+import { HomeTitle } from '@/components/HomeTitle';
 
 interface HomeProps {
   year: number;
@@ -81,38 +80,8 @@ interface HomeProps {
 //   }
 // }
 
-const getDateData = async () => {
-  const currentDate = new Date();
-  const year = currentDate.getFullYear();
-  const month = currentDate.getMonth() + 1;
-  const day = currentDate.getDate();
 
-  const rokuyou = calculateRokuyou(year, month, day);
 
-  return {
-    props: {
-      year,
-      month,
-      day,
-      rokuyou,
-    },
-  };
-};
-
-const HomeTitle = async () => {
-  const { props } = await getDateData();
-
-  return (
-<div className="h-1/2 text-neutral-100 bg-yellow-500 flex flex-col justify-center items-center gap-2">
-        <h1 className={`text-4xl font-bold ${lato.className}`}>
-          {props.year}/{props.month}/{props.day}
-        </h1>
-        <p className={`text-2xl ${potta_One.className} tracking-widest`}>
-          {props.rokuyou}
-        </p>
-      </div>
-  )
-}
 
 export default function Home () {
 
---
+++ components/HomeTitle.tsx
@@ -0,0 +1,38 @@
+
+import { calculateRokuyou } from "../libs/calculateRokuyou"
+
+import {lato, potta_One } from "../font";
+
+const getDateData = async () => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1;
+  const day = currentDate.getDate();
+
+  const rokuyou = calculateRokuyou(year, month, day);
+
+  return {
+    props: {
+      year,
+      month,
+      day,
+      rokuyou,
+    },
+  };
+};
+
+
+export const HomeTitle = async () => {
+  const { props } = await getDateData();
+
+  return (
+<div className="h-1/2 text-neutral-100 bg-yellow-500 flex flex-col justify-center items-center gap-2">
+        <h1 className={`text-4xl font-bold ${lato.className}`}>
+          {props.year}/{props.month}/{props.day}
+        </h1>
+        <p className={`text-2xl ${potta_One.className} tracking-widest`}>
+          {props.rokuyou}
+        </p>
+      </div>
+  )
+}

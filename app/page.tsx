1384c7f6ab04eb9b4632418d0ceda2ea9691ad8b
phihash <insharp0220@gmail.com>
2024-04-18 00:49:15 +0900

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { calculateRokuyou } from "../libs/calculateRokuyou"
-import { noto_sans_jp,geo, arvo ,lato, lexend, potta_One } from "../font";
+import {lato, potta_One } from "../font";
 
 interface HomeProps {
   year: number;
@@ -10,77 +10,77 @@ interface HomeProps {
 }
 
 
-async function getWikiData() {
-  try {
-    const response = await axios.get('https://ja.wikipedia.org/w/api.php', {
-      params: {
-        action: 'query',
-        format: 'json',
-        prop: 'extracts',
-        titles: '4月1日',
-        exintro: true,
-        explaintext: true,
-      },
-    });
-    console.log(response,"レスポン")
-    const data = response.data;
-    console.log(data,"data")
-    const pages = data.query.pages;
-    console.log(pages,"page")
-    const pageId = Object.keys(pages)[0];
-    console.log(pageId,"pageId")
-    const extract = pages[pageId].extract;
-    console.log(extract,"extrac")
+// async function getWikiData() {
+//   try {
+//     const response = await axios.get('https://ja.wikipedia.org/w/api.php', {
+//       params: {
+//         action: 'query',
+//         format: 'json',
+//         prop: 'extracts',
+//         titles: '4月1日',
+//         exintro: true,
+//         explaintext: true,
+//       },
+//     });
+//     console.log(response,"レスポン")
+//     const data = response.data;
+//     console.log(data,"data")
+//     const pages = data.query.pages;
+//     console.log(pages,"page")
+//     const pageId = Object.keys(pages)[0];
+//     console.log(pageId,"pageId")
+//     const extract = pages[pageId].extract;
+//     console.log(extract,"extrac")
 
-const events = extract.split('\n').filter((event : any) => {
-      const year = event.match(/^\d+年/);
-      return year && parseInt(year[0]) >= 1800;
-    });
+// const events = extract.split('\n').filter((event : any) => {
+//       const year = event.match(/^\d+年/);
+//       return year && parseInt(year[0]) >= 1800;
+//     });
 
-    return events.join('\n');
-  } catch (error) {
-    console.error(error);
-    throw new Error('An error occurred');
-  }
-}
+//     return events.join('\n');
+//   } catch (error) {
+//     console.error(error);
+//     throw new Error('An error occurred');
+//   }
+// }
 
-async function getBirthdaysData(month: number, day: number) {
-  try {
-    const response = await axios.get('https://ja.wikipedia.org/w/api.php', {
-      params: {
-        action: 'query',
-        format: 'json',
-        prop: 'extracts',
-        titles: `${month}月${day}日`,
-        exintro: true,
-        explaintext: true,
-      },
-    });
-    console.log(response,"レスポン")
-    const data = response.data;
-    console.log(data,"data")
-    const pages = data.query.pages;
-    console.log(pages,"page")
-    const pageId = Object.keys(pages)[0];
-    console.log(pageId,"pageId")
-    const extract = pages[pageId].extract;
-    console.log(extract,"extrac")
+// async function getBirthdaysData(month: number, day: number) {
+//   try {
+//     const response = await axios.get('https://ja.wikipedia.org/w/api.php', {
+//       params: {
+//         action: 'query',
+//         format: 'json',
+//         prop: 'extracts',
+//         titles: `${month}月${day}日`,
+//         exintro: true,
+//         explaintext: true,
+//       },
+//     });
+//     console.log(response,"レスポン")
+//     const data = response.data;
+//     console.log(data,"data")
+//     const pages = data.query.pages;
+//     console.log(pages,"page")
+//     const pageId = Object.keys(pages)[0];
+//     console.log(pageId,"pageId")
+//     const extract = pages[pageId].extract;
+//     console.log(extract,"extrac")
 
-    // 誕生日の人物のみを抽出
-    const regex = /(\d+年)(?:.+?)、(.+?)(?:（.+?）)?が生まれる。/g;
-    const birthdays = [];
-    let match;
-    while ((match = regex.exec(extract)) !== null) {
-      const [_, year, name] = match;
-      birthdays.push({ year, name });
-    }
+//     // 誕生日の人物のみを抽出
+//     const regex = /(\d+年)(?:.+?)、(.+?)(?:（.+?）)?が生まれる。/g;
+//     const birthdays = [];
+//     let match;
+//     while ((match = regex.exec(extract)) !== null) {
+//       const [_, year, name] = match;
+//       birthdays.push({ year, name });
+//     }
 
-    return birthdays;
-  } catch (error) {
-    console.error(error);
-    throw new Error('An error occurred');
-  }
-}
+//     return birthdays;
+//   } catch (error) {
+//     console.error(error);
+//     throw new Error('An error occurred');
+//   }
+// }
 
 const getDateData = async () => {
   const currentDate = new Date();
@@ -100,16 +100,18 @@ const getDateData = async () => {
   };
 };
 
-export default async function Home({ year, month, day, rokuyou }: HomeProps) {
-  const datadata  = await getDateData();
+export default async function Home() {
+  const { props } = await getDateData();
+
   return (
     <main className="h-screen">
       <div className="h-1/2 text-neutral-100 bg-yellow-500 flex flex-col justify-center items-center gap-2">
         <h1 className={`text-4xl font-bold ${lato.className}`}>
-            {datadata.props.year}/{datadata.props.month}/{datadata.props.day}
-          </h1>
-          <p className={`text-2xl ${potta_One.className} tracking-widest`}>
-            {datadata.props.rokuyou}</p>
+          {props.year}/{props.month}/{props.day}
+        </h1>
+        <p className={`text-2xl ${potta_One.className} tracking-widest`}>
+          {props.rokuyou}
+        </p>
       </div>
       <div className="h-1/2">
         {/* 下半分のコンテンツをここに追加 */}

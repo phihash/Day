8df971cbf4731cffc7ab8977f13ef84df050aaaa
phihash <insharp0220@gmail.com>
2024-04-18 23:32:55 +0900

    clientで動作するように

--- components/HomeTitle.tsx
+++ components/HomeTitle.tsx
@@ -1,9 +1,9 @@
 "use client"
 import { calculateRokuyou } from "../libs/calculateRokuyou"
-
 import {lato, potta_One } from "../font";
 
-const getDateData = async () => {
+
+export const HomeTitle = async () => {
   const currentDate = new Date();
   const year = currentDate.getFullYear();
   const month = currentDate.getMonth() + 1;
@@ -11,27 +11,13 @@ const getDateData = async () => {
 
   const rokuyou = calculateRokuyou(year, month, day);
 
-  return {
-    props: {
-      year,
-      month,
-      day,
-      rokuyou,
-    },
-  };
-};
-
-
-export const HomeTitle = async () => {
-  const { props } = await getDateData();
-
   return (
 <div className="h-1/2 text-neutral-100 bg-yellow-500 flex flex-col justify-center items-center gap-2">
         <h1 className={`text-4xl font-bold ${lato.className}`}>
-          {props.year}/{props.month}/{props.day}
+          {year}/{month}/{day}
         </h1>
         <p className={`text-2xl ${potta_One.className} tracking-widest`}>
-          {props.rokuyou}
+          {rokuyou}
         </p>
       </div>
   )

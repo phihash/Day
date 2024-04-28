ddb6d493d8b724f092b8462c8f7ca423fe3cbc5d
phihash <insharp0220@gmail.com>
2024-04-29 01:56:17 +0900

    wip

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,13 @@
 "use client"
 import { IoPerson } from "react-icons/io5";
 
-
 export const Button  =  () => {
 
   return (
     <div  className="">
         <button className={`text-3xl  p-6 border rounded-xl`}>
           <IoPerson />
+          人物
         </button>
       </div>
   )
--- components/HomeTitle.tsx
+++ components/HomeTitle.tsx
@@ -4,19 +4,25 @@ import { Button } from "../components/Button";
 import Link from "next/link";
 
 
-export const HomeTitle :React.FC<HomeTitleProps> =  ({date}) => {
-
+export default function HomeTitle  ({date} : HomeTitleProps) {
   return (
 <div  className="h-screen text-neutral-100 bg-yellow-500 flex flex-col justify-center items-center gap-2">
         <h1 className={`text-4xl font-bold ${lato.className}`}>
           {String(date)}年
         </h1>
         <Link href={`/person/${date}`}>
-        <Button />
+         <Button />
+        </Link>
+        <Link href={`/person/${date}`}>
+         <Button />
+        </Link>
+        <Link href={`/person/${date}`}>
+         <Button />
         </Link>
       </div>
   )
 }
+
 
 interface HomeTitleProps {
   date: Number;

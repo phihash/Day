8fe8e4f8b3525c95f36626e99a327a63fac46ed4
phihash <insharp0220@gmail.com>
2024-04-28 19:31:52 +0900

    404ページ作成

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+import { noto_sans_jp } from "../font";
+
+
+function NotFoundPage() {
+  return (
+    <div className="bg-yellow-50 h-screen flex items-center justify-center p-8">
+      <div>
+        <p
+          className={`md:text-3xl text-2xl font-semibold ${noto_sans_jp.className}  mb-6`}
+        >
+          ページが見つかりません
+        </p>
+        <p
+          className={`md:text-8xl text-8xl font-semibold ${noto_sans_jp.className} w-max mx-auto`}
+        >
+          404
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default NotFoundPage;

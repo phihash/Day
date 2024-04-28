7c60ff78c2b056d5ede8d18c3a9287e80ca58604
phihash <insharp0220@gmail.com>
2024-04-28 19:32:46 +0900

    Button

---
+++ components/Button.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { IoPerson } from "react-icons/io5";
+
+
+export const Button  =  () => {
+
+  return (
+    <div  className="">
+        <button className={`text-3xl  p-6 border rounded-xl`}>
+          <IoPerson />
+        </button>
+      </div>
+  )
+}
